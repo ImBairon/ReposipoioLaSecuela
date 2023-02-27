a7b2ca8419e9db3ca8a01086583592913b218aeb
Bairon <baironft10@gmail.com>
2023-02-27 14:09:27 -0500

    cambio2

---
+++ OperadoresAritmeticos.py
@@ -0,0 +1,23 @@
+#libreria
+import math
+#Adquirir Informacion 
+a = int(input("Digite el numero 1:"))
+print(f"El numero a es: {a}")
+b = int(input("digite el numeo 2:"))
+print(f"el numero a es: {b}" )
+#Proceso
+c = a + b 
+d = a - b 
+e = a * b 
+f = a / b 
+g = a ** b 
+h = math.sqrt(a) 
+#Salida
+print(F"El resultado de la suma es: {c}")  
+print(F"El resultado de la resta es: {d}")
+print(F"El resultado de la multiplicacion es: {e}")
+print(F"El resultado de la division es: {f}")
+print(F"El resultado de la potencia es: {g}")
+print(F"El resultado de la Raiz es: {h}")       
+
+print("Hola")
