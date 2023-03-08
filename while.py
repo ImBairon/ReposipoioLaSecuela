242483c4d37d8517c4dab379260423895792ed40
Bairon <baironft10@gmail.com>
2023-03-08 14:23:24 -0500

    opciones con string

---
+++ while.py
@@ -0,0 +1,30 @@
+#variable 
+"""contador = 0
+
+#estructura
+while contador <=4:
+    if contador ==3:
+        print("Unisangil")
+        break
+    print(f"contador ={contador}")
+    contador +=1 
+print("salio del while")
+
+#Break y continue
+
+while contador < 20:
+    contador +=1
+    if contador < 15:
+        continue
+    print(contador)"""
+
+texto = "Estoy en clase de  algoritmos en unisangil"
+#Buscar
+print("clase" in texto)
+print(texto.title()) #Titulo
+print(texto.upper()) #Mayusculas
+print(texto.lower()) #Minusculas
+print(texto.count("a")) #Contar
+
+
+
