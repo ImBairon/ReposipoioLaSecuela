2bff9fe1cd5b2018e4fb82ff25ea07930feaf958
Bairon <baironft10@gmail.com>
2023-03-24 14:12:43 -0500

    Punto 6 con condicional en la salida de consola

---
+++ punto6.py
@@ -0,0 +1,7 @@
+import time
+
+for i in range(1, 6):
+    print(i, "Mississippi")
+    time.sleep(2) # Esta funcion hace que el programa tenga en cuenta la manera de contar en missisippi.
+if i==5:
+    print(f"¡Listo o no, ahí voy! \U0001F479") #coloque un if para que imprima el mensaje cuando termine de contarse los 5 misisippi
