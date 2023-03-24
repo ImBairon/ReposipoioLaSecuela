9242ebf8d2a80991f3534fc4dfedda808da2a036
Bairon <baironft10@gmail.com>
2023-03-24 14:45:35 -0500

    punto 7 con validacione en el .lower

---
+++ punto7.py
@@ -0,0 +1,6 @@
+while True:
+    palabra = input("Ingresa una palabra: ")
+    
+    if palabra.lower() == "chupacabra": #Para validar el uso de mayus o miunus al inicio de la palabra
+        print("¡Haz dejado en ciclo con éxito!")
+        break
